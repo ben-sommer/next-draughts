17cae6d35cbae92ada53266c7188d11e191d8d59
Ben Sommer <32485726+ben-sommer@users.noreply.github.com>
2022-05-05 19:50:26 +0100

    chore: add nuxt app boilerplate

---
+++ components/Counter.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    {{ counter }}
+    <button @click="counter++">+</button>
+    <button @click="counter--">-</button>
+  </div>
+</template>
+
+<script setup>
+const counter = useState("count", () => 0);
+</script>
